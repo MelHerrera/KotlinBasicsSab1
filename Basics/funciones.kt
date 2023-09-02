f259811cb0cc5f6a722e38c8a169fdf3ac4ac677
mherrera <melkinantonioh@gmail.com>
2023-09-02 14:08:12 -0600

    funciones de extension

--- Basics/funciones.kt
+++ Basics/funciones.kt
@@ -4,6 +4,10 @@ var miSuma1 = sumar(100)
 
     println(miSuma)
     println(miSuma1)
+
+    var saludo = "hola".firstLetterToUpper()
+
+    println(saludo)
 }
 
 fun sumar():Int{
@@ -12,4 +16,16 @@ fun sumar():Int{
 
 fun sumar(num:Int):Int{
     return num + 10
+}
+
+fun String.firstLetterToUpper():String{
+    //1. capturar la primera letra
+    //2. convertirla a mayuscula
+    //3. Unir toda la palabra
+    //4. Devolver el resultado
+
+    var firstLetter:String = this.get(0).toString()
+    var lettersRight:String = this.substring(1)
+
+    return firstLetter.toUpperCase() + lettersRight
 }
